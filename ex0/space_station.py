08fdbf3fa96d36ba9f66e8acd042de036fecb5c7
tmagoudi <tmagoudi@f2r3s10.paris.42.school>
2026-07-19 18:39:04 +0200

    first push

---
+++ ex0/space_station.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import Optional
+
+class Station(BaseModel):
+    station_id: str = Field(min_length=3, max_length=10)
+    name: str = Field(min_length=1, max_length = 50)
+    crew_size: int = Field(ge=1, le= 20)
+    power_level: float = Field(ge= 0.0,le = 100.0)
+    oxygen_level: float = Field(ge= 0.0, le= 100.0)
+    last_maintenance: datetime
+    is_operational: bool = True
+    notes: Optional[str] = Field(
+        default=None,
+        max_length=200
+    )
+
+def main() -> None:
+    print("Space Station Data Validation")
+    print("========================================")
+    try:
+        V_station = Station(station_id="000", name="International Space Station", crew_size=6, power_level= 85.5 , oxygen_level= 92.3, last_maintenance=datetime.now())
+        print("Valid station created:")
+        print("Station Name:", V_station.name)
+        print("Crew", V_station.crew_size)
+        print("Power", V_station.power_level)
+        print("Oxygen", V_station.oxygen_level)
+    except ValueError as e:
+        print(e)
+    print("========================================")
+    
+
+
+if __name__ == '__main__':
+    main()
